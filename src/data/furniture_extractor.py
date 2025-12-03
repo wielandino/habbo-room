fba1aaac9d8828d59126e4917edd5e5a059a85c5
wielandino <wieland-f.wueller@outlook.de>
2025-12-03 20:31:21 +0100

    added furniture asset extractor

--- src/data/furniture_extractor.py
+++ src/data/furniture_extractor.py
@@ -1,10 +1,13 @@
 import os
+from data.furniture_asset import FurnitureAsset
 import definitions
 from bs4 import BeautifulSoup
-
+import pygame
 
 class FurnitureExtractor:
     path: str = f"{definitions.ROOT_DIR}/src/data/furnitures/"
+    xml_path: str = "xml/"
+    image_path: str = "images/"
 
     def __init__(self, type: str):
         self.type = type
@@ -12,41 +15,27 @@ class FurnitureExtractor:
         self.__set_path(type)
 
     def __set_path(self, type: str):
-        self.path = os.path.join(self.path, f"{type}/xml/")
+        self.path = os.path.join(self.path, f"{type}/")
 
-    def __load_furniture_manifest_xml(self):
-        with open(os.path.join(self.path, "manifest.xml"), "r", encoding="utf-8") as file:
+    def __load_furniture_manifest_xml(self) -> BeautifulSoup:
+        with open(os.path.join(self.path, f"{self.xml_path}manifest.xml"), "r", encoding="utf-8") as file:
             content = file.read()
 
-        soup = BeautifulSoup(content, "xml")
-        return soup
-
-    def __load_furniture_asset_xml(self):
-        with open(os.path.join(self.path, "assets.xml"), "r", encoding="utf-8") as file:
-            content = file.read()
-
-        soup = BeautifulSoup(content, "xml")
-        return soup
-    
-    def __load_furniture_visualization_xml(self):
-        with open(os.path.join(self.path, "visualization.xml"), "r", encoding="utf-8") as file:
-            content = file.read()
-
-        soup = BeautifulSoup(content, "xml")
-        return soup
-
-    def extract(self):
-        manifest_soup = self.__load_furniture_manifest_xml()
-
-        if not self.__is_manifest_valid(manifest_soup):
-            return None
-        
-        asset_soup = self.__load_furniture_asset_xml()
-        visualization_soup = self.__load_furniture_visualization_xml()
-
-    
+        return BeautifulSoup(content, "xml")
         
 
+    def __load_furniture_asset_xml(self) -> BeautifulSoup:
+        with open(os.path.join(self.path, f"{self.xml_path}assets.xml"), "r", encoding="utf-8") as file:
+            content = file.read()
+
+        return BeautifulSoup(content, "xml")
+      
+    
+    def __load_furniture_visualization_xml(self) -> BeautifulSoup:
+        with open(os.path.join(self.path, f"{self.xml_path}visualization.xml"), "r", encoding="utf-8") as file:
+            content = file.read()
+
+        return BeautifulSoup(content, "xml")
 
     def __is_manifest_valid(self, soup: BeautifulSoup) -> bool:
         # Check if furniture has assets and visualization xml
@@ -57,5 +46,39 @@ class FurnitureExtractor:
             return False
 
         return True
+    
+    def extract(self):
+        manifest_xml = self.__load_furniture_manifest_xml()
+
+        if not self.__is_manifest_valid(manifest_xml):
+            return None
+        
+        asset_xml = self.__load_furniture_asset_xml()
+        visualization_xml = self.__load_furniture_visualization_xml()
+
+        for asset in asset_xml.find_all("asset"):
+            name = asset.get("name")
+            flip_h = asset.get("flipH") == "1"
+            source = asset.get("source")
+            offset_x = int(asset.get("x", "0"))
+            offset_y = int(asset.get("y", "0"))
+        
+            if source is None:
+                png_path = os.path.join(self.path, f"{self.image_path}{name}.png")
+            else:
+                png_path = os.path.join(self.path, f"{self.image_path}{source}.png")
+
+            sprite = None
+            if os.path.exists(png_path):
+                sprite = pygame.image.load(png_path).convert_alpha()
+
+            asset = FurnitureAsset(
+                flip_h=flip_h,
+                type=self.type,
+                offset_x=offset_x,
+                offset_y=offset_y,
+                source_name=name,
+                sprite=sprite
+            )
 
     
